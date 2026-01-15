43ccd7233eb3b44fd62c1c5a2f2801d664d87f96
Radek Burget <burgetr@fit.vutbr.cz>
2026-01-15 11:29:28 +0100

    CLI runner fix

--- cli.sh
+++ cli.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-if [ $# -ne 1 ]; then
-    echo "Usage: cli.sh <server_url>"
+if [ $# -ne 2 ]; then
+    echo "Usage: cli.sh <server_url> <repository_id>"
     exit 1
 fi
 
 SERVER_URL="$1"
+REPOSITORY_ID="$2"
 
-python -i flclient/cli.py "$SERVER_URL"
+python -i flclient/cli.py "$SERVER_URL" "$REPOSITORY_ID"
